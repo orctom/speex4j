74fc8e11e550407b3d0c422217d9ecc4f9a78fea
Hao <orctom@gmail.com>
2017-07-19 23:50:56 +0800

    linux support

--- src/main/jni/build_linux.sh
+++ src/main/jni/build_linux.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 echo "Compiling..."
-gcc  -shared -fPIC \
+gcc -shared -fPIC \
   -c com_orctom_speex4j_SpeexDecoder.h com_orctom_speex4j_SpeexDecoder.c slots.h slots.c \
-  -I/$JAVA_HOME/include \
-  -I/$JAVA_HOME/include/linux/ \
+  -I $JAVA_HOME/include \
+  -I $JAVA_HOME/include/linux/ \
   -I speex-1.2.0/include/ \
   -I .
 
 echo "Linking..."
-gcc  -shared -fPIC  -o libspeexcodec.so \
-  -c com_orctom_speex4j_SpeexDecoder.h com_orctom_speex4j_SpeexDecoder.c slots.h slots.c \
-  -I/$JAVA_HOME/include \
-  -I/$JAVA_HOME/include/linux/ \
+gcc -shared -fPIC -o libspeexcodec.so \
+  com_orctom_speex4j_SpeexDecoder.o slots.o \
+  -I $JAVA_HOME/include \
+  -I $JAVA_HOME/include/linux/ \
   -I speex-1.2.0/include/ \
   -L speex-1.2.0/lib \
+  -l speex \
   -I .
 
-cp libspeexcodec.so ../resources/
+mv libspeexcodec.so ../resources/
 echo "Done".
